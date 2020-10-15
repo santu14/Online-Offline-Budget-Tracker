0dd7ea3a3d4a579c1ffdbab7f1815321ef0271b4
Santiago Solana <santiago.sjs@gmail.com>
2020-10-15 12:44:26 -0400

    working app online and offline

--- public/db.js
+++ public/db.js
@@ -1,6 +1,7 @@
 let db;
-
+// db request for new budget database
 const request = indexedDB.open("budget", 1);
+
 
 request.onupgradeneeded = ({ target }) => {
   const db = target.result;
@@ -15,12 +16,44 @@ request.onsuccess = ({ target }) => {
 };
 
 request.onerror = (event) => {
-  // log error here
   console.log("Woops! " + event.target.errorCode);
 };
 
-const storeData = record => {
+function storeData(record) {
     const transaction = db.transaction(["pending"], "readwrite");
     const pendingStore = transaction.objectStore("pending");
     pendingStore.add(record);
 }
+
+const checkDatabase = () => {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+    const getAll = pendingStore.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+          fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json",
+            },
+          })
+            .then((response) => response.json())
+            .then(() => {
+           
+              const transaction = db.transaction(["pending"], "readwrite");
+             
+              const pendingStore = transaction.objectStore("pending");
+      
+              pendingStore.clear();
+              
+            });
+        }
+      };
+
+    
+};
+
+window.addEventListener("online", checkDatabase);
